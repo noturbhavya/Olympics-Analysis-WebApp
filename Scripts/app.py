93290720239614f04ba878b078776c102439e199
Bhavya <bhavyapatela100@gmail.com>
2025-09-21 17:49:31 +0530

    Initial commit - Olympics Analysis WebApp

--- Scripts/app.py
+++ Scripts/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import preprocess, helper
+import Scripts.preprocess as preprocess, Scripts.helper as helper
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
